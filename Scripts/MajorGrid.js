39aec78945353b8d7a8504c5c8103005d3a2c3d4
00Greenwood <marcus_greenwood@hotmail.co.uk>
2020-05-17 12:01:16 +0100

    Move some code around

--- Scripts/MajorGrid.js
+++ Scripts/MajorGrid.js
@@ -2,30 +2,22 @@
 // A major grid is a width by height grid of minor grids of size height by width.
 class MajorGrid {
   constructor(width, height) {
-    this.width = width; // minor grid height
-    this.height = height; // minor grid width
+    this.width = width;
+    this.height = height;
+    this.minorWidth = height;
+    this.minorHeight = width;
     this.numbers = width * height; // Grid contains 1 to numbers
     // Fill the grid with minor grids.
     this.grid = [];
     for (let i = 0; i < this.width; ++i) {
       this.grid[i] = [];
       for (let j = 0; j < this.height; ++j) {
-        // Swap width and height to make sudoku square.
-        this.grid[i][j] = new MinorGrid(this.height, this.width, i, j);
+        this.grid[i][j] = new MinorGrid(this.minorWidth, this.minorHeight, i, j);
       }
     }
     // Permute the row and columns, mixing up the grid.
     for (let i = 0; i < Math.pow(this.numbers, 4); ++i) {
       this.permute();
-    }
-    // Add some starting numbers, this.number - 1 is the minimum number required. 
-    while (this.countInputs() < this.numbers - 1) {
-      let i = randomInt(this.width);
-      let j = randomInt(this.height);
-      let k = randomInt(this.height);
-      let l = randomInt(this.width);
-      let minorGrid = this.grid[i][j];
-      minorGrid.inputGrid[k][l] = minorGrid.grid[k][l];
     }
   }
 
@@ -83,8 +75,8 @@ class MajorGrid {
   // Permute a pair of rows in a random minor grid.
   permuteMinorRow() {
     let i = randomInt(this.height);
-    let j = randomInt(this.width);
-    let k = randomInt(this.width);
+    let j = randomInt(this.minorHeight);
+    let k = randomInt(this.minorHeight);
     for (let l = 0; l < this.width; ++l) {
       this.grid[l][i].permuteRows(j, k);
     }
@@ -93,8 +85,8 @@ class MajorGrid {
   // Permute a pair of columns in a random minor grid.
   permuteMinorColumn() {
     let i = randomInt(this.width);
-    let j = randomInt(this.height);
-    let k = randomInt(this.height);
+    let j = randomInt(this.minorWidth);
+    let k = randomInt(this.minorWidth);
     for (let l = 0; l < this.height; ++l) {
       this.grid[i][l].permuteColumns(j, k);
     }
@@ -102,8 +94,8 @@ class MajorGrid {
 
   // Permute a pair of numbers.
   permuteNumbers() {
-    let i = randomInt(this.numbers);
-    let j = randomInt(this.numbers);
+    let i = 1 + randomInt(this.numbers);
+    let j = 1 + randomInt(this.numbers);
     for (let k = 0; k < this.width; ++k) {
       for (let l = 0; l < this.height; ++l) {
         this.grid[k][l].permuteNumbers(i, j);
@@ -119,6 +111,34 @@ class MajorGrid {
       }
     }
     return sum;
+  }
+
+  getInputNumber(i, j) {
+    let x = Math.floor(i / this.width);
+    let y = Math.floor(j / this.height);
+    let minorX = i % this.width;
+    let minorY = j % this.height;
+    return this.grid[x][y].inputGrid[minorX][minorY];
+  }
+
+  setInputNumber(i, j, number) {
+    let x = Math.floor(i / this.width);
+    let y = Math.floor(j / this.height);
+    let minorX = i % this.width;
+    let minorY = j % this.height;
+    this.grid[x][y].inputGrid[minorX][minorY] = number;
+  }
+
+  getNumber(i, j) {
+    let x = Math.floor(i / this.width);
+    let y = Math.floor(j / this.height);
+    let minorX = i % this.width;
+    let minorY = j % this.height;
+    return this.grid[x][y].grid[minorX][minorY];
+  }
+
+  hasUniqueSolution() {
+    return true;
   }
 
 }
--- Scripts/main.js
+++ Scripts/main.js
@@ -1,5 +1,19 @@
 var height = 2;
 var width = 2;
+var totalHeight = width * height;
+var totalWidth =  height * width;
+var sudokuNumbers = totalHeight;
 var sudokuGrid = new MajorGrid(width, height);
+// Create a solvable sudoku.
+// Add some starting numbers, height * width - 1 is the minimum number of preset input required. 
+while (sudokuGrid.countInputs() < height * width - 1) {
+  let i = randomInt(totalWidth);
+  let j = randomInt(totalHeight);
+  sudokuGrid.setInputNumber(i, j, sudokuGrid.getNumber(i, j));
+}
+// Check the sudoku is unique, if not, add a number.
+//while (!sudokuGrid.hasUniqueSolution()) {
+
+//}
 
 draw(sudokuGrid);
